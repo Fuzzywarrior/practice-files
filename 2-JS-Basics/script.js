ebd180c878111b22573ae39dc2d7aa1c88cee032
Matthew Hamel <fuzzywarrior@outlook.com>
2017-10-19 20:45:38 -0700

    Added script.js

---
+++ 2-JS-Basics/script.js
@@ -0,0 +1,52 @@
+script.js/*
+var name = 'John';
+var lastName = 'Smith';
+var age = 26;
+var fullAge = true;
+
+console.log(name);
+console.log(lastName);
+console.log(age);
+console.log(fullAge);
+*/
+
+// Lecture: Variables 2
+/*
+var name = 'John';
+var age = 26;
+var job, isMarried;
+
+job = 'teacher';
+isMarried = true;
+
+
+console.log(name + age);
+console.log(name + ' is a ' + age + ' year old ' + job + ' is he married? ' + isMarried + '.');
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' year old ' + job + ' is he married? ' + isMarried + '.');
+
+var lastName = prompt('What is the last name?');
+
+console.log(lastName);
+
+alert(name + ' is a ' + age + ' year old ' + job + ' is he married? ' + isMarried + '.');
+*/
+
+// Lecture: operators
+
+var now = 2016;
+var birthYear = now - 26;
+
+birthYear = now - 26 * 2;
+//2016 - 52
+//1964
+
+console.log(birthYear);
+
+
+
+
+
