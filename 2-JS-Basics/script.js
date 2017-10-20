56ef31b7eeddf649689f32b0f85a68c2623bd0b6
Matthew Hamel <fuzzywarrior@outlook.com>
2017-10-19 21:14:43 -0700

    Operators
    Stupid crap with some operators

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -46,6 +46,20 @@ birthYear = now - 26 * 2;
 
 console.log(birthYear);
 
+var ageJohn = 30;
+var ageMark = 30;
+
+ageJohn = ageMark = (3 + 5) * 4 - 6;
+//ageJohn = ageMark = 26
+//ageJohn = 26
+
+ageJohn ++;
+ageMark *= 2;
+ageMark = ageMark * 2;
+
+console.log(ageJohn);
+console.log(ageMark);
+
 
 
 
