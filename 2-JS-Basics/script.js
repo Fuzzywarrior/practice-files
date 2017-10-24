aefd442dca15c248278ae57b17edf36fc428ab4c
Matthew Hamel <fuzzywarrior@outlook.com>
2017-10-23 17:41:34 -0700

    If and Switch commands

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -49,7 +49,7 @@ console.log(birthYear);
 
 ////////////////////////////////
 // Lecture: if/else statements
-
+/*
 var name = 'John';
 var age = 26;
 var isMarried = 'no';
@@ -71,8 +71,36 @@ if(isMarried) {
 if (23 == '23'){
     console.log ('Print something...');
 }
+*/
+
+////////////////////////////////////
+// Lecture: boolean logic and switch
+
+var age = 20;
+
+if (age < 20) {
+    console.log('John is a teenager');
+} else if (age >= 20 && age < 30 ) {
+    console.log('John is a young man.');
+} else {
+    console.log('John is a man.');
+}
 
 
+var job = 'teacher';
+job = prompt('What does John do?');
 
-
+switch (job) {
+case 'teacher':
+    console.log('John teaches kids.');
+    break;
+case 'driver':
+    console.log('John drives a cab in Lisbon');
+    break;
+case 'cop':
+    console.log('John helps fight crime.');
+    break;
+default:
+    console.log('John does something else.');
+}
 
