7c8b2c5d4b330be97259ee9b116600e9afe63606
Matthew Hamel <fuzzywarrior@outlook.com>
2017-11-02 22:43:24 -0700

    this stuff

--- 3-How-JS-Works/script.js
+++ 3-How-JS-Works/script.js
@@ -66,12 +66,35 @@ function third() {
 
 ///////////////////////////////////////
 // Lecture: The this keyword
+console.log(this);
+
+function calculateAge(year) {
+    console.log(2016 - year);
+    console.log(this)
+}
 
 
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2017 - this.yearOfBirth);
 
+        function innerFunction() {
+            console.log(this);
+        }
+        //innerFunction();
+    }
+}
 
+john.calculateAge();
 
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984,
+};
 
-
-
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
 
